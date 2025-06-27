147af065bde18d2dd74db2e76a6bec5eaf2d4deb
wgibbs322 <wgibbs322@gmail.com>
2025-06-27 10:48:43 -0700

    m

--- model/addadminTransaction.js
+++ model/addadminTransaction.js
@@ -6,9 +6,9 @@ const transactionSchema = new mongoose.Schema({
   description: { type: String, required: true },
   amount: { type: Number, required: true },
   balanceAfter: { type: Number, required: true },
-  status: { type: String, default: 'Pending' },
-  createdAt: { type: Date, default: Date.now }
-});
+  status: { type: String, default: 'Pending' }
+}, { timestamps: true });
+
 
 // ✅ Check if already compiled
 export default mongoose.models.Transaction || mongoose.model('Transaction', transactionSchema);
